1f4421739723d196ffcf3cfe452a4dd978336bc0
wocaowuqimg <759296837@qq.com>
2022-07-18 17:22:42 +0800

    提交文件

---
+++ js/change.js
@@ -0,0 +1,25 @@
+function change() {
+    var str = document.getElementById("bt").innerHTML;
+    var txt = str.replace("乌克兰载有", "人民日报八论六中全会");
+    document.getElementById("bt").innerHTML = txt;
+
+    var str1 = document.getElementById("bt1").innerHTML;
+    var txt1 = str1.replace("乌克兰载有", "伊藤美诚又被打哭了");
+    document.getElementById("bt1").innerHTML = txt1;
+
+    var str2 = document.getElementById("bt2").innerHTML;
+    var txt2 = str2.replace("乌克兰载有", "封丘营养餐痛哭校长回应20年合同");
+    document.getElementById("bt2").innerHTML = txt2;
+
+    var str3 = document.getElementById("bt3").innerHTML;
+    var txt3 = str3.replace("乌克兰载有", "世乒赛现场发生种族歧视");
+    document.getElementById("bt3").innerHTML = txt3;
+
+    var str4 = document.getElementById("bt4").innerHTML;
+    var txt4 = str.replace("乌克兰载有", "澳门政府:已将周焯华带回警局调查");
+    document.getElementById("bt4").innerHTML = txt4;
+
+    var str5 = document.getElementById("bt5").innerHTML;
+    var txt5 = str5.replace("乌克兰载有", "超级变异株来袭 疫苗还有效吗？");
+    document.getElementById("bt5").innerHTML = txt5;
+}
